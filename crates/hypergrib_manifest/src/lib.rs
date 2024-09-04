941672474806491952414ac1468afdd391970182
Jack Kelly <jack@openclimatefix.org>
2024-09-04 11:44:02 +0100

    Basic parse_idx works! With simple test

---
+++ crates/hypergrib_manifest/src/lib.rs
@@ -0,0 +1,54 @@
+use anyhow;
+
+#[derive(PartialEq, Debug, serde::Deserialize)]
+struct IdxRecord {
+    msg_id: u32,
+    byte_offset: u32,
+    init_date: String, // TODO: Convert to datetime
+    nwp_variable: String,
+    vertical_level: String,
+    step: String,
+    member: String,
+}
+
+fn parse_idx(b: &[u8]) -> anyhow::Result<Vec<IdxRecord>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b':')
+        .has_headers(false)
+        .from_reader(b);
+    let mut records = vec![];
+    for result in rdr.deserialize() {
+        records.push(result?);
+    }
+    Ok(records)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_idx() -> anyhow::Result<()> {
+        let idx_text = "\
+1:0:d=2017010100:HGT:10 mb:anl:ENS=low-res ctl
+2:50487:d=2017010100:TMP:10 mb:anl:ENS=low-res ctl
+3:70653:d=2017010100:RH:10 mb:anl:ENS=low-res ctl
+4:81565:d=2017010100:UGRD:10 mb:anl:ENS=low-res ctl
+";
+        let records = parse_idx(idx_text.as_bytes())?;
+        assert_eq!(records.len(), 4);
+        assert_eq!(
+            records[0],
+            IdxRecord {
+                msg_id: 1,
+                byte_offset: 0,
+                init_date: String::from("d=2017010100"),
+                nwp_variable: String::from("HGT"),
+                vertical_level: String::from("10 mb"),
+                step: String::from("anl"),
+                member: String::from("ENS=low-res ctl"),
+            }
+        );
+        Ok(())
+    }
+}
