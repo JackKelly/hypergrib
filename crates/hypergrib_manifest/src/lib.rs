d291e468d7208a63d521c77c4b37c571976fb1eb
Jack Kelly <jack@openclimatefix.org>
2024-09-04 19:46:28 +0100

    Adding more TODOs to comments

--- crates/hypergrib_manifest/src/lib.rs
+++ crates/hypergrib_manifest/src/lib.rs
@@ -34,18 +34,17 @@ fn read_idx_into_duck_db(filename: String) -> anyhow::Result<()> {
             init_time TIMESTAMP,
             nwp_variable VARCHAR,
             vertical_level VARCHAR,
-            step VARCHAR,
-            member VARCHAR,
+            step_raw VARCHAR,
+            step INTERVAL,
+            ens_member VARCHAR,
             );
         ",
     )?;
     // TODO: Change to enums: nwp_variable, vertical_level, ensemble_member
-    // TODO: Change to INTERVAL: step
-    // TODO: Add filename
-    // TODO: Convert init_time to DATETIME
+    // TODO: Add filename of GRIB file (with full path)
 
     conn.execute_batch(
-        r"COPY grib_message FROM 
+        r"COPY grib_message (msg_id, byte_offset, init_time, nwp_variable, vertical_level, step_raw, ens_member) FROM 
             '/home/jack/dev/rust/hypergrib/gec00.t00z.pgrb2af000.idx'
             (
               DELIMITER ':',
@@ -55,7 +54,11 @@ fn read_idx_into_duck_db(filename: String) -> anyhow::Result<()> {
               TIMESTAMPFORMAT 'd=%Y%m%d%H'
             );
           ",
-    )?; // TODO: Pass in filename!
+    )?;
+    // TODO: Pass in filename argument!
+    // TODO: Convert step_raw to step, maybe by first storing step_raw as VARCHAR, replacing 'anl'
+    // with '0', taking just the first two characters of the string, converting that to an int, and
+    // passing that to [`to_hours(integer)`](https://duckdb.org/docs/sql/functions/interval#to_hoursinteger)
 
     // Print some test data:
     let mut stmt = conn.prepare("SELECT * FROM grib_message WHERE msg_id < 5")?;
