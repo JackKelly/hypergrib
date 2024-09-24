6a12ee40531b9991d408afec18114eaa7fa91520
Jack Kelly <jack@openclimatefix.org>
2024-09-24 17:36:57 +0100

    New design moved into code

--- crates/hypergrib_manifest/src/datasets/gefs.rs
+++ crates/hypergrib_manifest/src/datasets/gefs.rs
@@ -1,9 +1,8 @@
 //! NOAA's Global Ensemble Forecast System (GEFS).
 //! https://registry.opendata.aws/noaa-gefs
 
+use crate::{Dataset, Manifest};
 use anyhow;
-use chrono::prelude::*;
-use chrono::TimeDelta;
 
 #[derive(PartialEq, Debug, serde::Deserialize)]
 struct IdxRecord {
@@ -29,61 +28,23 @@ fn parse_idx(b: &[u8]) -> anyhow::Result<Vec<IdxRecord>> {
     Ok(records)
 }
 
-struct GefsKey {
-    init_time: DateTime<Utc>,
-    ensemble_member: u16, // Or an `enum EnsembleMember {Control, Perturbed(u16)}`
-    forecast_step: TimeDelta,
-    nwp_variable: String,   // TODO: Use NWPVariable enum?
-    vertical_level: String, // TODO: Use VerticalLevel enum?
+struct GefsDataset {
+    manifest: Manifest,
 }
 
-impl crate::Key for GefsKey {
-    fn to_path(&self) -> object_store::path::Path {
-        let mut parts = Vec::<object_store::path::PathPart>::with_capacity(3);
-        let init_hour = format!("{:02}", self.init_time.hour());
-
-        // First part of the Path:
-        parts.push(self.init_time.format("gefs.%Y%m%d").to_string().into());
-
-        // Second part of the Path:
-        parts.push(init_hour.clone().into());
-
-        // Third part of the Path:
-        let ensemble_member = if self.ensemble_member == 0 {
-            "gec00".to_string()
-        } else {
-            format!("gef{:02}", self.ensemble_member)
-        };
-        let forecast_step = if self.forecast_step == TimeDelta::zero() {
-            "anl".to_string()
-        } else {
-            format!("f{:03}", self.forecast_step.num_hours())
-        };
-        parts.push(
-            format!(
-                "{ensemble_member}.t{init_hour}z.pgrb2a{forecast_step}",
-                ensemble_member = ensemble_member,
-                init_hour = init_hour,
-                forecast_step = forecast_step,
-            )
-            .into(),
-        );
-        object_store::path::Path::from_iter(parts)
+impl Dataset for GefsDataset {
+    fn ingest_grib_idx(&mut self, idx_path: Path, idx_contents: &[u8]) -> Result {
+        // insert `idx_path` into `self.dataset.paths`, and get a ref to the `path` in `paths`
+        // for use in the `Chunk`.
     }
-}
 
-impl TryFrom<object_store::path::Path> for GefsKey {
-    type Error = &'static str;
-
-    fn try_from(value: object_store::path::Path) -> Result<Self, Self::Error> {
-        todo!();
+    fn manifest_as_ref(&self) -> &Manifest {
+        &self.manifest
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Key;
-
     use super::*;
 
     #[test]
@@ -107,28 +68,6 @@ mod tests {
                 forecast_step: String::from("anl"),
                 ensemble_member: String::from("ENS=low-res ctl"),
             }
-        );
-        Ok(())
-    }
-
-    #[test]
-    fn test_key_to_path() -> anyhow::Result<()> {
-        let k = GefsKey {
-            // Note that the string on the line below includes minutes, even though the GEFS
-            // idx files do not contain minutes. This is because `chrono::NaiveDateTime::parse_from_str`
-            // throws an error if minutes aren't present in the string :(.
-            init_time: NaiveDateTime::parse_from_str("201701010000", "%Y%m%d%H%M")
-                .expect("parse datetime")
-                .and_utc(),
-            ensemble_member: 0,
-            forecast_step: TimeDelta::hours(6),
-            nwp_variable: "HGT".to_string(),
-            vertical_level: "10 mb".to_string(),
-        };
-        let p = k.to_path();
-        assert_eq!(
-            p,
-            object_store::path::Path::from("gefs.20170101/00/gec00.t00z.pgrb2af006")
         );
         Ok(())
     }
--- crates/hypergrib_manifest/src/lib.rs
+++ crates/hypergrib_manifest/src/lib.rs
@@ -2,6 +2,144 @@
 
 pub mod datasets;
 
-pub(crate) trait Key: TryFrom<object_store::path::Path> {
-    fn to_path(&self) -> object_store::path::Path;
+use std::{
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
+
+use chrono::{DateTime, TimeDelta, Utc};
+use gribberish::templates::product::parameters::meteorological;
+
+// TODO: Replace this with Enums from gribberish.
+#[derive(PartialEq, Eq, Hash)]
+enum EnsembleMember {
+    Control,
+    Perturbed(u16),
+    Mean,
+    Spread,
+}
+
+// TODO: Replace this with Enums from gribberish. See https://github.com/mpiannucci/gribberish/issues/59
+// TODO: Include all parameters listed for GEFS here:
+// - https://www.nco.ncep.noaa.gov/pmb/products/gens/gep01.t00z.pgrb2a.0p50.f003.shtml
+// - https://www.nco.ncep.noaa.gov/pmb/products/gens
+/// Adapted from https://www.nco.ncep.noaa.gov/pmb/products/gens/gec00.t00z.pgrb2a.0p50.f000.shtml
+#[derive(PartialEq, Eq, Hash)] // PartialEq, Eq, and Hash are required for HashMap keys.
+enum Parameter {
+    // The unit is after the underscore
+    GeopotentialHeight_gpm,
+    Temperature_K,
+    RelativeHumidity_percent,
+    UComponentOfWind_meters_per_sec,
+    VComponentOfWind_meters_per_sec,
+    VerticalVelocityAKAPressure_Pa_per_sec,
+}
+
+// TODO: Replace this with Enums from gribberish. See https://github.com/mpiannucci/gribberish/issues/59
+/// Adapted from https://www.nco.ncep.noaa.gov/pmb/products/gens
+#[derive(PartialEq, Eq, Hash)] // PartialEq, Eq, and Hash are required for HashMap keys.
+enum VerticalLevel {
+    Mb10,
+    Mb50,
+    Mb100,
+    Mb200,
+    Mb250,
+    Mb300,
+    Mb400,
+    Mb500,
+    Mb700,
+    Mb850,
+    Mb925,
+    Mb1000,
+    Surface,
+    OneCentimeterBelowGround,
+    TwoMetersAboveGround,
+    TenMetersAboveGround,
+    EntireAtmosphere,
+    OneHundredAndEightyMbAboveGround,
+    MeanSeaLevel,
+    TopOfAtmosphere,
+}
+
+#[derive(PartialEq, Eq, Hash)] // PartialEq, Eq, and Hash are required for HashMap keys.
+struct Key {
+    reference_time: DateTime<Utc>,
+    ensemble_member: EnsembleMember,
+    forecast_step: TimeDelta,
+    parameter: Parameter, // `Variable` is an enum
+    vertical_level: VerticalLevel, // `VerticalLevel` is an enum?
+                          // Also for consideration:
+                          // provider: Provider,  // e.g. NOAA, UKMetOffice, ECMWF, etc.
+                          // nwp_model: NWPModel,  // e.g. GFS, GEFS, UKV, etc.
+                          // or maybe combine `provider` and `nwp_model` into a single Enum e.g. UKMO_UKV, etc?
+}
+
+/// The location of a GRIB message.
+struct MessageLocation {
+    path: Arc<object_store::path::Path>,
+    byte_offset: u32,
+    msg_length: u32,
+    // TODO: Store a reference to coord labels for x and y?
+    // TODO: Maybe a ref to a struct which holds lots of metadata about this grib message such as:
+    // - coord labels for x and y
+    // - NWP model version
+    // - other metadata?
+}
+
+// TODO: Implement `struct CoordLabels` and `SortedVecSet<T>`:
+// struct SortedVecSet<T>(Vec<T>);
+//
+// impl<T> SortedVecSet<T> {
+//   /// Insert only if a duplicate doesn't exist. Sorts after insertion.
+//   fn insert(t: T) -> Result<DuplicateExists>;
+// }
+//
+// struct NwpCoordLabels {
+//   // We're using `SortedVecSet` (not `BTreeSet`) because the most performance-sensitive
+//   // part of the process is looking up a coord label given an integer index.
+//   // And the only way to do that with a `BTreeSet` is to first iterate over the elements.
+//   init_time: SortedVecSet<Datetime>,
+//   ensemble_member: SortedVecSet<u16>,
+//   forecast_step: SortedVecSet<Timedelta>,
+//   nwp_variable: SortedVecSet<Variable>,
+//   vertical_level: SortedVecSet<VerticalLevel>,
+// }
+//
+
+struct Manifest {
+    // TODO: Add coord_labels: CoordLabels,
+    // Store the paths once, so we only have one Arc per Path.
+    // Each path in `paths` will be relative to `base_path`.
+    base_path: object_store::path::Path,
+    paths: HashSet<Arc<object_store::path::Path>>,
+    manifest: HashMap<Key, MessageLocation>,
+    // Maybe we also want a `manifest_index` which maps integer indexes to `MessageLocation`
+    // but let's make a start with the design below and benchmark it.
+}
+
+impl Manifest {
+    fn insert(&mut self, key: Key, chunk: MessageLocation) -> Result<(), AlreadyExistsError> {
+        // Insert into `manifest` and update `coord_labels` iff the new coord doesn't exist yet.
+    }
+
+    fn coord_labels_to_chunk(&self, key: &Key) -> Option<MessageLocation> {
+        self.manifest[key]
+    }
+
+    fn index_locs_to_key(&self, index: &[u64]) -> Option<Key> {
+        // get key by looking up the appropriate coord labels in self.coord_labels.
+        // Returns `None` if any index is out of bounds (which is the same semantics as `Vec::get`).
+        // Although maybe it'd be better to return a custom `Error` so we can say which dim
+        // is out of bounds? Or if there are the wrong number of dims in the `index`?
+        Some(key)
+    }
+}
+
+trait Dataset {
+    fn ingest_grib_idx(
+        &mut self,
+        idx_path: object_store::path::Path,
+        idx_contents: &[u8],
+    ) -> Result;
+    fn manifest_as_ref(&self) -> &Manifest;
 }
