a2fd20b252d88c95582c795d4ae5d2df0acf634c
Jack Kelly <jack@openclimatefix.org>
2024-10-16 13:41:27 +0100

    Basic sketch works. Test passes!

--- crates/grib_tables/src/lib.rs
+++ crates/grib_tables/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use std::collections::HashMap;
 
 // From https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table4-0.shtml
@@ -27,7 +28,16 @@ struct IndividualEnsembleForecastAtHorizontalLevel {
 
 //---------------------- PARAMETER DATABASE: --------------------------
 
-#[derive(Hash, Eq, PartialEq, Copy, Clone)]
+#[derive(Hash, Eq, PartialEq, Copy, Clone, Debug, derive_more::Display)]
+#[display(
+    "{}, {}, {}, {}, {}, {}",
+    product_discipline,
+    parameter_category,
+    parameter_number,
+    originating_center,
+    local_table_version,
+    master_table_version
+)]
 struct NumericId {
     product_discipline: u8,
     parameter_category: u8,
@@ -37,9 +47,19 @@ struct NumericId {
     master_table_version: u8,
 }
 
-#[derive(Hash, Eq, PartialEq, Clone)]
+#[derive(Hash, Eq, PartialEq, Clone, Debug, derive_more::Display)]
 struct Abbreviation(String);
 
+#[derive(Clone, Debug, derive_more::Display, PartialEq, Eq)]
+#[display(
+    "({}, {}, {}, {}, {}, {})",
+    numeric_id,
+    description,
+    note,
+    unit,
+    abbreviation,
+    status
+)]
 struct Parameter {
     numeric_id: NumericId,
     description: String,
@@ -49,11 +69,13 @@ struct Parameter {
     status: Status,
 }
 
+#[derive(Clone, Debug, derive_more::Display, PartialEq, Eq)]
 enum Status {
     Operational,
     Deprecated,
 }
 
+// TODO: Use `pin` and NonNull pointers: https://doc.rust-lang.org/nightly/std/pin/index.html#a-self-referential-struct
 struct ParameterDatabase {
     params: Vec<Parameter>,
 
@@ -64,9 +86,10 @@ struct ParameterDatabase {
     abbrev_lookup: HashMap<Abbreviation, usize>,
 }
 
+#[derive(thiserror::Error, Debug, derive_more::Display)]
 enum ParameterInsertionError {
-    NumericKeyAlreadyExists,
-    AbbrevKeyAlreadyExists,
+    NumericKeyAlreadyExists(Parameter),
+    AbbrevKeyAlreadyExists(Parameter),
 }
 
 impl ParameterDatabase {
@@ -78,18 +101,18 @@ impl ParameterDatabase {
         }
     }
 
-    fn insert(&mut self, parameter: Parameter) -> Result<(), (Parameter, ParameterInsertionError)> {
+    fn insert(&mut self, parameter: Parameter) -> Result<(), ParameterInsertionError> {
         let index = self.params.len();
 
         // Insert into self.numeric_lookup if the key doesn't exist yet:
         if self.numeric_lookup.contains_key(&parameter.numeric_id) {
-            return Err((parameter, ParameterInsertionError::NumericKeyAlreadyExists));
+            return Err(ParameterInsertionError::NumericKeyAlreadyExists(parameter));
         }
         self.numeric_lookup.insert(parameter.numeric_id, index);
 
         // Insert into self.numeric_lookup if the key doesn't exist yet:
         if self.abbrev_lookup.contains_key(&parameter.abbreviation) {
-            return Err((parameter, ParameterInsertionError::AbbrevKeyAlreadyExists));
+            return Err(ParameterInsertionError::AbbrevKeyAlreadyExists(parameter));
         }
         self.abbrev_lookup
             .insert(parameter.abbreviation.clone(), index);
@@ -115,10 +138,12 @@ impl ParameterDatabase {
 
 #[cfg(test)]
 mod test {
+    use anyhow::Context;
+
     use super::*;
 
     #[test]
-    fn insert_and_retreive() {
+    fn insert_and_retreive() -> anyhow::Result<()> {
         let numeric_id = NumericId {
             product_discipline: 0,
             parameter_category: 0,
@@ -138,5 +163,19 @@ mod test {
             abbreviation: abbreviation.clone(),
             status: Status::Operational,
         };
+
+        let mut param_db = ParameterDatabase::new();
+        assert_eq!(param_db.len(), 0);
+
+        param_db.insert(param.clone())?;
+        assert_eq!(param_db.len(), 1);
+
+        let retrieved_param = param_db
+            .abbreviation_to_parameter(&param.abbreviation)
+            .context("Failed to map from abbrev to param")?;
+
+        assert_eq!(param, *retrieved_param);
+
+        Ok(())
     }
 }
