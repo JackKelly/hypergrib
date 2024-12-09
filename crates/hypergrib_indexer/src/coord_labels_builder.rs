efacd53bf955aa160509d4646f1a39f316536697
Jack Kelly <jack@openclimatefix.org>
2024-12-09 18:13:33 +0000

    Change to BTreeSets

--- crates/hypergrib_indexer/src/coord_labels_builder.rs
+++ crates/hypergrib_indexer/src/coord_labels_builder.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{
+    collections::{BTreeSet, HashSet},
+    sync::Arc,
+};
 
 use chrono::{DateTime, TimeDelta, Utc};
 use hypergrib::CoordLabels;
@@ -10,11 +13,11 @@ pub(crate) struct CoordLabelsBuilder {
     grib_base_path: object_store::path::Path,
     idx_store: Arc<dyn ObjectStore>,
     idx_base_path: object_store::path::Path,
-    reference_datetime: HashSet<DateTime<Utc>>,
-    ensemble_member: HashSet<String>,
-    forecast_step: HashSet<TimeDelta>,
-    parameter: HashSet<String>,
-    vertical_level: HashSet<String>,
+    reference_datetime: BTreeSet<DateTime<Utc>>,
+    ensemble_member: BTreeSet<String>,
+    forecast_step: BTreeSet<TimeDelta>,
+    parameter: BTreeSet<String>,
+    vertical_level: BTreeSet<String>,
 }
 
 impl CoordLabelsBuilder {
@@ -29,11 +32,11 @@ impl CoordLabelsBuilder {
             grib_base_path,
             idx_store,
             idx_base_path,
-            reference_datetime: HashSet::new(),
-            ensemble_member: HashSet::new(),
-            forecast_step: HashSet::new(),
-            parameter: HashSet::new(),
-            vertical_level: HashSet::new(),
+            reference_datetime: BTreeSet::new(),
+            ensemble_member: BTreeSet::new(),
+            forecast_step: BTreeSet::new(),
+            parameter: BTreeSet::new(),
+            vertical_level: BTreeSet::new(),
         }
     }
 
@@ -64,24 +67,33 @@ impl CoordLabelsBuilder {
         }
     }
 
-    pub(crate) fn grib_store(&self) -> &dyn ObjectStore {
+    pub(crate) fn grib_store(&self) -> &Arc<dyn ObjectStore> {
         &self.grib_store
     }
 
-    pub(crate) fn grib_base_path(&self) -> &str {
-        self.grib_base_path.as_ref()
+    pub(crate) fn grib_base_path(&self) -> &object_store::path::Path {
+        &self.grib_base_path
     }
 
-    pub(crate) fn idx_store(&self) -> &dyn ObjectStore {
+    pub(crate) fn idx_store(&self) -> &Arc<dyn ObjectStore> {
         &self.idx_store
     }
 
-    pub(crate) fn idx_base_path(&self) -> &str {
-        self.idx_base_path.as_ref()
+    pub(crate) fn idx_base_path(&self) -> &object_store::path::Path {
+        &self.idx_base_path
+    }
+
+    pub(crate) fn insert_reference_datetime(&mut self, datetime: DateTime<Utc>) -> bool {
+        self.reference_datetime.insert(datetime)
     }
 }
 
-fn set_to_sorted_vec<T: Ord>(set: HashSet<T>) -> Vec<T> {
+fn set_to_sorted_vec<T, S>(set: S) -> Vec<T>
+where
+    T: Ord,
+    S: IntoIterator,
+    Vec<T>: FromIterator<<S as IntoIterator>::Item>,
+{
     let mut v: Vec<T> = set.into_iter().collect();
     v.sort();
     v
--- crates/hypergrib_indexer/src/datasets/gefs.rs
+++ crates/hypergrib_indexer/src/datasets/gefs.rs
@@ -1,6 +1,9 @@
+use anyhow::Context;
+use chrono::{DateTime, NaiveDate, Utc};
 use hypergrib::{CoordLabels, GetCoordLabels};
 
 use crate::coord_labels_builder::CoordLabelsBuilder;
+use list_with_depth::list_with_depth;
 
 const BUCKET_URL: &str = "s3://noaa-gefs-pds";
 const SKIP_SIGNATURE: bool = true;
@@ -17,41 +20,69 @@ impl Gefs {
         })
     }
 
-    /// The reference datetimes in extracted from the first two parts of the path, for example:
+    /// The reference datetimes are extracted from the first two parts of the path, for example:
     /// `gefs.20241204/00/`.
-    async fn get_reference_datetimes(&self) -> anyhow::Result<()> {
-        let list = self
-            .coord_labels_builder
-            .grib_store()
-            .list_with_delimiter(None)
-            .await?;
+    async fn get_reference_datetimes(&mut self) -> anyhow::Result<()> {
+        let store = self.coord_labels_builder.idx_store().clone();
+        let prefix = self.coord_labels_builder.idx_base_path();
+        let list = list_with_depth(store, Some(prefix), 1).await?;
 
-        let n = list.common_prefixes.len();
-        println!(
-            "Number of ListResult.common_prefixes: {n}, the last is {:?}",
-            list.common_prefixes[n - 1]
-        );
-
-        // TODO:
-        // - For each directory like `gefs.20241204`, find all the init hours by calling
-        //   list_with_delimiter again. In parallel.
-        // - Convert to `DateTime<Utc>` using code like this:
-        //   https://github.com/JackKelly/hypergrib/issues/22#issuecomment-2517163383
-        // - Insert these into `self.coord_labels_builder.reference_datetime` HashSet.
-        //   HashSet<T> is Send and Sync if T is Send and Sync.
-        // I've looked at the AWS S3 API docs and I can't see any way to use wildcards.
-        // So maybe I should make a utility function which lists all objects at a particular
-        // depth. e.g. depth 1 would be equivalent of `ls *`; depth 2 would be like `ls */*` etc.
-        // And return a stream?
+        for prefix in list.common_prefixes.iter() {
+            let datetime = path_to_reference_datetime(prefix)?;
+            let datetime_is_unique = self
+                .coord_labels_builder
+                .insert_reference_datetime(datetime);
+            assert!(
+                datetime_is_unique,
+                "Duplicate reference datetime! {datetime}"
+            );
+        }
 
         Ok(())
     }
 }
 
 impl GetCoordLabels for Gefs {
-    async fn get_coord_labels(self) -> anyhow::Result<CoordLabels> {
+    async fn get_coord_labels(mut self) -> anyhow::Result<CoordLabels> {
         self.get_reference_datetimes().await?;
         // TODO: Append all coords to the coord_labels_builder!
         Ok(self.coord_labels_builder.build())
+    }
+}
+
+/// Convert the first two parts of a path to a reference datetime.
+/// For example, `gefs.20191122/18` becomes 2019-11-22T18:00.
+fn path_to_reference_datetime(path: &object_store::path::Path) -> anyhow::Result<DateTime<Utc>> {
+    let parts: Vec<_> = path.parts().take(2).collect();
+    let error_context = |s| format!("{s} when parsing path: '{path}'");
+    let date = NaiveDate::parse_from_str(parts[0].as_ref(), "gefs.%Y%m%d").with_context(|| {
+        error_context("Failed to convert date component of NWP reference datetime")
+    })?;
+    let hour: u32 = parts[1]
+        .as_ref()
+        .parse()
+        .with_context(|| error_context("Hour of the NWP init could not be parsed into a u32"))?;
+    match date.and_hms_opt(hour, 0, 0) {
+        Some(dt) => Ok(dt.and_utc()),
+        None => Err(anyhow::format_err!(error_context(
+            "Invalid NWP reference hour"
+        ))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_path_to_reference_datetime() -> anyhow::Result<()> {
+        let path = object_store::path::Path::from("gefs.20191122/18");
+        let result = path_to_reference_datetime(&path)?;
+        assert_eq!(
+            result,
+            DateTime::parse_from_rfc3339("2019-11-22T18:00:00Z")?
+        );
+        Ok(())
     }
 }
